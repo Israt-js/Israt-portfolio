0c8702b888e4c9e9fba18b999fa23e52d3128ab2
israt.js <israt8291jahan@gmail.com>
2024-08-29 23:20:55 +0600

    first commit

---
+++ src/Component/About/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+    <div className="">
+  
+    </div>
+    );
+  };
+  
+  export default About;
+  
---
+++ src/Component/Project/Project.jsx
@@ -0,0 +1,10 @@
+const Project = () => {
+    return (
+    <div className="">
+  
+    </div>
+    );
+  };
+  
+  export default Project;
+  
